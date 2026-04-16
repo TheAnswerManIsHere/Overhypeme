8cf94b9e3d4956ff928de9da3b8c4c02218f85ce
david2384 <45488748-david2384@users.noreply.replit.com>
2026-04-16 19:03:43 +0000

    Git commit prior to merge

--- artifacts/api-server/src/lib/zazzle.ts
+++ artifacts/api-server/src/lib/zazzle.ts
@@ -6,21 +6,24 @@ export interface ZazzleLinkOptions {
   returnUrl?: string;
 }
 
-export async function buildZazzleUrl(opts: ZazzleLinkOptions = {}): Promise<string> {
+export async function buildZazzleUrl(
+  opts: ZazzleLinkOptions = {},
+): Promise<string> {
   const [at, rf, ax, sr, cg, ed, tc] = await Promise.all([
-    getConfigString("zazzle_at", "238499514566968751"),
-    getConfigString("zazzle_rf", "238499514566968751"),
-    getConfigString("zazzle_ax", "DesignBlast"),
+    getConfigString("zazzle_at", ""),
+    getConfigString("zazzle_rf", ""),
+    getConfigString("zazzle_ax", ""),
     getConfigString("zazzle_sr", ""),
     getConfigString("zazzle_cg", ""),
-    getConfigString("zazzle_ed", "true"),
+    getConfigString("zazzle_ed", ""),
     getConfigString("zazzle_tc", ""),
   ]);
 
   const params = new URLSearchParams();
+  params.set("at", at);
   params.set("rf", rf);
   params.set("ax", ax);
-  if (sr) params.set("sr", sr);
+  params.set("sr", sr);
   if (cg) params.set("cg", cg);
   params.set("ed", ed);
   if (opts.returnUrl) params.set("continueUrl", opts.returnUrl);
